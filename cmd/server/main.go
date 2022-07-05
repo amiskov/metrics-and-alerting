c76e1090d660d685e95a3a4e341e9470abc80076
Andrey Miskov <amiskov@gmail.com>
2022-07-05 09:39:22 +0300

    Handle CLI params for the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+var port string
+
+func init() {
+	// CLI options
+	serverPort := flag.Int("port", 8080, "server port")
+	flag.Parse()
+	port = ":" + strconv.Itoa(*serverPort)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
@@ -11,5 +22,5 @@ func main() {
 		log.Println(req.URL.Path)
 		w.Write([]byte("Got it!" + req.URL.Path))
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
