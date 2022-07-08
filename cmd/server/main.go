f816e81746036b7cb93a4b2446facef7d2ae1b71
Andrey Miskov <amiskov@gmail.com>
2022-07-08 18:07:00 +0300

    Remove flags from server CLI

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/amiskov/metrics-and-alerting/cmd/server/router"
 )
 
 var port string
 
-func init() {
-	// CLI options
-	serverPort := flag.Int("port", 8080, "server port")
-	port = ":" + strconv.Itoa(*serverPort)
-}
+// func init() {
+// 	// CLI options
+// 	serverPort := flag.Int("port", 8080, "server port")
+// 	port = ":" + strconv.Itoa(*serverPort)
+// }
 
 func main() {
-	flag.Parse()
+	// flag.Parse()
 	router := router.NewRouter()
-	fmt.Printf("Server has been started at %s\n", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	// fmt.Printf("Server has been started at %s\n", port)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
