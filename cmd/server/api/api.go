67d27d462d31ea83ce75cf42b87b7bb3a777408b
Andrey Miskov <amiskov@gmail.com>
2022-07-12 11:18:28 +0300

    Move router into separate file

--- cmd/server/api/api.go
+++ cmd/server/api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/middleware"
 	"log"
 	"net/http"
 
@@ -34,29 +33,4 @@ func New(s Storage) *metricsAPI {
 func (api *metricsAPI) Run(port string) {
 	fmt.Printf("Server has been started at %s\n", port)
 	log.Fatal(http.ListenAndServe(port, api.Router))
-}
-func (api *metricsAPI) MountHndlrs(handler *chi.Mux) {
-	api.Router = handler
-}
-
-func (api *metricsAPI) mountHandlers() {
-	api.Router.Use(middleware.RequestID)
-	api.Router.Use(middleware.RealIP)
-	api.Router.Use(middleware.Logger)
-	api.Router.Use(middleware.Recoverer)
-
-	api.Router.Route("/value", func(r chi.Router) {
-		r.Get("/{metricType}/{metricName}", api.getMetric)
-	})
-
-	api.Router.Route("/update", func(r chi.Router) {
-		r.Post("/{metricType}/", handleNotFound)
-		r.Post("/{metricType}/{metricName}/", handleNotImplemented)
-		r.Post("/{metricType}/{metricName}/{metricValue}", api.upsertMetric)
-	})
-
-	api.Router.Route("/", func(r chi.Router) {
-		r.Get("/", api.getMetricsList)
-		r.Post("/*", handleNotFound)
-	})
 }
