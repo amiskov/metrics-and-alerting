57fb871530df2ace7eefc39f3c3a77357f6f3606
Andrey Miskov <amiskov@gmail.com>
2022-07-26 09:49:28 +0300

    Add ENV config with defaults

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,51 +2,42 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/amiskov/metrics-and-alerting/cmd/agent/api"
 	"github.com/amiskov/metrics-and-alerting/cmd/agent/service"
+	"github.com/caarlos0/env"
 )
 
-var serverURL string
-var pollInterval time.Duration
-var reportInterval time.Duration
-
-func init() {
-	// CLI options
-	sendProtocol := flag.String("protocol", "http", "server protocol")
-	sendHost := flag.String("host", "127.0.0.1", "server host")
-	sendPort := flag.Int("port", 8080, "server port")
-	pollIntervalNumber := flag.Int("poll", 2, "poll interval in seconds")
-	reportIntervalNumber := flag.Int("report", 10, "report interval in seconds")
-	flag.Parse()
-
-	serverURL = *sendProtocol + "://" + *sendHost + ":" + strconv.Itoa(*sendPort)
-
-	pollInterval = time.Duration(time.Duration(*pollIntervalNumber) * time.Second)
-	reportInterval = time.Duration(time.Duration(*reportIntervalNumber) * time.Second)
+type config struct {
+	Address        string        `env:"ADDRESS" envDefault:"http://localhost:8080"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
 }
 
 func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+		panic(err)
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 
 	updater := service.New()
 	reporter := api.New(updater)
 
 	finished := make(chan bool, 1) // buffer of 2 for updater and reporter
-	time.Sleep(5 * time.Second)
-	go updater.Run(ctx, finished, pollInterval)
-	go reporter.RunJSON(ctx, finished, reportInterval, serverURL)
+	time.Sleep(1 * time.Second)
+	go updater.Run(ctx, finished, cfg.PollInterval)
+	go reporter.RunJSON(ctx, finished, cfg.ReportInterval, cfg.Address)
 
 	log.Println("Agent has been started.")
-	log.Printf("Sending to: %v. Poll: %v. Report: %v.\n", serverURL, pollInterval, reportInterval)
+	log.Printf("Sending to: %v. Poll: %v. Report: %v.\n", cfg.Address, cfg.PollInterval, cfg.ReportInterval)
 
 	// Managing user signals
 	osSignalCtx, stopBySyscall := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/amiskov/metrics-and-alerting
 
 go 1.18
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/go-chi/chi v1.5.4
+)
+
+require github.com/stretchr/testify v1.8.0 // indirect
