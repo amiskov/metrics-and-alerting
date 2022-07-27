8bf678bce59d44b0495bdb2395417f09a444d756
Andrey Miskov <amiskov@gmail.com>
2022-07-27 10:46:30 +0300

    Simplify server store

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,25 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"time"
 
 	"github.com/amiskov/metrics-and-alerting/cmd/server/api"
-	"github.com/amiskov/metrics-and-alerting/cmd/server/store/inmemory"
+	"github.com/amiskov/metrics-and-alerting/cmd/server/store"
 	"github.com/caarlos0/env"
 )
 
 type config struct {
 	Address string `env:"ADDRESS" envDefault:"localhost:8080"`
+	// 0s STORE_INTERVAL stores metrics immediately.
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
+	// Don't store metrics if `StoreFile` is empty.
+	StoreFile string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+	// Load previously saved metrics from file if `Restore` is true.
+	Restore bool `env:"RESTORE" envDefault:"true"`
 }
 
 func main() {
 	cfg := config{}
 	if err := env.Parse(&cfg); err != nil {
-		fmt.Printf("%+v\n", err)
-		panic(err)
+		if err != nil {
+			log.Fatalln("Parsing CLI params failed.", err)
+		}
 	}
 
-	storage := inmemory.New()
+	storage, err := store.New(store.StoreCfg{
+		StoreFile:     cfg.StoreFile,
+		StoreInterval: cfg.StoreInterval,
+		Restore:       cfg.Restore,
+	})
+	if err != nil {
+		log.Fatalln("Creating server store failed.", err)
+	}
 
 	metricsAPI := api.New(storage)
 	metricsAPI.Run(cfg.Address)
--- cmd/server/store/store.go
+++ cmd/server/store/store.go
@@ -1,3 +1,103 @@
 package store
 
-// TODO: Add storage error types
+import (
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	sm "github.com/amiskov/metrics-and-alerting/cmd/server/models"
+	"github.com/amiskov/metrics-and-alerting/internal/models"
+)
+
+type StoreCfg struct {
+	StoreInterval time.Duration
+	StoreFile     string
+	Restore       bool
+}
+
+type metricsDB map[string]models.Metrics
+
+type store struct {
+	mx            *sync.Mutex
+	Metrics       metricsDB
+	StoreInterval time.Duration
+	StoreFile     *os.File
+}
+
+func New(cfg StoreCfg) (*store, error) {
+	shouldUseStoreFile := cfg.StoreFile != ""
+	shouldRestoreFromFile := shouldUseStoreFile && cfg.Restore
+
+	// File Storage
+	var file *os.File
+	if shouldUseStoreFile {
+		file, err := os.OpenFile(cfg.StoreFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+		defer file.Close()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Preload or create metrics DB
+	var metrics metricsDB
+	if shouldRestoreFromFile {
+		metrics = restoreFromFile(file)
+	} else {
+		metrics = make(metricsDB)
+	}
+
+	return &store{
+		mx:            new(sync.Mutex),
+		StoreInterval: cfg.StoreInterval,
+		StoreFile:     file,
+		Metrics:       metrics,
+	}, nil
+}
+
+func restoreFromFile(file *os.File) map[string]models.Metrics {
+	// TODO: Load from file
+	return make(metricsDB)
+}
+
+func (s *store) Update(m models.Metrics) error {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+
+	switch m.MType {
+	case "counter":
+		if _, ok := s.Metrics[m.ID]; ok {
+			*s.Metrics[m.ID].Delta += *m.Delta
+		} else {
+			s.Metrics[m.ID] = m
+		}
+	case "gauge":
+		s.Metrics[m.ID] = m
+	default:
+		return sm.ErrorUnknownMetricType
+	}
+
+	return nil
+}
+
+func (s store) GetAll() []models.Metrics {
+	var metrics []models.Metrics
+	for _, m := range s.Metrics {
+		metrics = append(metrics, m)
+	}
+	sort.Slice(metrics, func(i, j int) bool {
+		return metrics[i].ID < metrics[j].ID
+	})
+	return metrics
+}
+
+func (s store) Get(metricType string, metricName string) (models.Metrics, error) {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+
+	metric, ok := s.Metrics[metricName]
+	if !ok {
+		return metric, sm.ErrorMetricNotFound
+	}
+	return metric, nil
+}
