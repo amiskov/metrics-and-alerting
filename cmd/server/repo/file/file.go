4e5dfe92688e26a0a687e5e258e8947f564f05db
Andrey Miskov <amiskov@gmail.com>
2022-08-13 13:35:21 +0300

    Fix restoring from file

--- cmd/server/repo/file/file.go
+++ cmd/server/repo/file/file.go
@@ -35,18 +35,19 @@ func New(filePath string) (*fileStorage, closer, error) {
 	return &s, fileCloser, nil
 }
 
-func (s *fileStorage) addFileStorage(name string) (func() error, error) {
+func (fs *fileStorage) addFileStorage(name string) (func() error, error) {
 	file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0o777)
 	if err != nil {
 		return nil, err
 	}
-	s.file = file
+	fs.file = file
 	return file.Close, nil
 }
 
-func (s *fileStorage) Restore(inmemDB models.MetricsDB) error {
-	storedMetrics := []models.Metrics{}
-	dec := json.NewDecoder(s.file)
+// Decodes JSON from the file and writes it to the given `MetricsDB`.
+func (fs *fileStorage) Restore(inmemDB models.MetricsDB) error {
+	storedMetrics := models.MetricsDB{}
+	dec := json.NewDecoder(fs.file)
 	err := dec.Decode(&storedMetrics)
 
 	if errors.Is(err, io.EOF) {
@@ -58,39 +59,39 @@ func (s *fileStorage) Restore(inmemDB models.MetricsDB) error {
 		for _, m := range storedMetrics {
 			inmemDB[m.ID] = m
 		}
-		log.Printf("Metrics data restored from %s", s.file.Name())
+		log.Printf("Metrics data restored from %s", fs.file.Name())
 		return nil
 	}
 
 	return err
 }
 
-func (s *fileStorage) Dump(inmemDB models.MetricsDB) error {
-	if _, err := s.file.Stat(); err != nil {
+func (fs *fileStorage) Dump(inmemDB models.MetricsDB) error {
+	if _, err := fs.file.Stat(); err != nil {
 		log.Println("Can't save to file:", err)
 		return err
 	}
 
-	if err := s.file.Truncate(0); err != nil {
+	if err := fs.file.Truncate(0); err != nil {
 		log.Println("Can't truncate file contents:", err)
 		return err
 	}
 
-	if _, err := s.file.Seek(0, 0); err != nil {
+	if _, err := fs.file.Seek(0, 0); err != nil {
 		log.Println("Can't set the file offset:", err)
 		return err
 	}
 
-	if err := json.NewEncoder(s.file).Encode(inmemDB); err != nil {
-		log.Printf("Can't store to file `%s`: %s.\n", s.file.Name(), err)
+	if err := json.NewEncoder(fs.file).Encode(inmemDB); err != nil {
+		log.Printf("Can't store to file `%s`: %s.\n", fs.file.Name(), err)
 		return err
 	}
 
-	log.Printf("Metrics dumped into file `%s`.\n", s.file.Name())
+	log.Printf("Metrics dumped into file `%s`.\n", fs.file.Name())
 
 	return nil
 }
 
-func (s fileStorage) Ping(ctx context.Context) error {
+func (fs fileStorage) Ping(ctx context.Context) error {
 	return nil
 }
