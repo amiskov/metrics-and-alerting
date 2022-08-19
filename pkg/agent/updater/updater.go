37b35c658a67848b62c71e0fb4ade082096b98bb
Andrey Miskov <amiskov@gmail.com>
2022-08-19 09:34:27 +0300

    update agent in goroutines

--- pkg/agent/updater/updater.go
+++ pkg/agent/updater/updater.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"runtime"
+	"sync"
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
@@ -42,6 +43,8 @@ func New(ctx context.Context, terminated chan<- bool, db *inmem.DB, pollInterval
 func (u *updater) Run() {
 	ticker := time.NewTicker(u.pollInterval)
 
+	wg := new(sync.WaitGroup)
+
 	go func() {
 		<-u.ctx.Done()
 		ticker.Stop()
@@ -50,17 +53,34 @@ func (u *updater) Run() {
 	}()
 
 	for range ticker.C {
-		u.updateMetrics()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			u.updateMemStats()
+			logger.Log(u.ctx).Info("Mem stats was updated.")
+		}()
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			u.updateVirtualMem()
+			logger.Log(u.ctx).Info("Virtual mem was updated.")
+		}()
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			u.updateCPU()
+			logger.Log(u.ctx).Info("CPU was updated.")
+		}()
+
+		wg.Wait()
+		log.Println("Metrics updated.")
 	}
 }
 
-func (u *updater) updateMetrics() {
+func (u *updater) updateMemStats() {
 	runtime.ReadMemStats(u.memStats)
-
-	vMem, err := mem.VirtualMemory()
-	if err != nil {
-		logger.Log(u.ctx).Fatal("can't create virtual memory stats object: %v", err)
-	}
 
 	u.updateGauge("Alloc", float64(u.memStats.Alloc))
 	u.updateGauge("BuckHashSys", float64(u.memStats.BuckHashSys))
@@ -93,16 +113,25 @@ func (u *updater) updateMetrics() {
 	u.updateCounter("PollCount")
 
 	u.updateGauge("RandomValue", rand.Float64()) // nolint: gosec
+}
 
-	u.updateGauge("TotalMemory", float64(vMem.Total))
-	u.updateGauge("FreeMemory", rand.Float64())
-	if cpu, err := cpu.Percent(0, true); err != nil {
-		logger.Log(u.ctx).Error("failed getting CPU info: %v", err)
-	} else {
-		u.updateGauge("CPUutilization1", cpu[1])
+func (u *updater) updateVirtualMem() {
+	vMem, err := mem.VirtualMemory()
+	if err != nil {
+		logger.Log(u.ctx).Error("can't create virtual memory stats object: %v", err)
+		return
 	}
+	u.updateGauge("TotalMemory", float64(vMem.Total))
+	u.updateGauge("FreeMemory", float64(vMem.Available))
+}
 
-	log.Println("Metrics has been updated.")
+func (u *updater) updateCPU() {
+	cpu, err := cpu.Percent(0, true)
+	if err != nil {
+		logger.Log(u.ctx).Error("failed getting CPU info: %v", err)
+		return
+	}
+	u.updateGauge("CPUutilization1", cpu[1])
 }
 
 func (u *updater) updateCounter(id string) {
